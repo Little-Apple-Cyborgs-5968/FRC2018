077ff0880cb1b1a305af832a07fe3cef74e69cb2
Matt Choi <mchoi1218@gmail.com>
2018-02-07 16:56:05 -0600

    Matt's Grabber code

---
+++ src/org/usfirst/frc/team5968/robot/Grabber.java
@@ -0,0 +1,47 @@
+package org.usfirst.frc.team5968.robot;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+
+public class Grabber implements IGrabber{
+
+    private DoubleSolenoid piston;
+    private PistonState pistonState; 
+    
+    
+    public Grabber (){
+        DoubleSolenoid piston = new DoubleSolenoid(0,0); //add ports later
+        pistonState = PistonState.OPEN;
+    }
+    
+    private void grab() {      
+        pistonState = PistonState.OPEN;
+        
+    }
+
+    private void release() {
+        pistonState = PistonState.CLOSED;
+        
+    }
+
+    @Override
+    public void toggleGrabbing() {
+        if (pistonState == PistonState.OPEN){
+            release();
+        }else{
+            grab();
+        }
+        
+    }
+    
+    
+    @Override
+    public void periodic() {
+        if (pistonState == PistonState.OPEN){
+            piston.set(DoubleSolenoid.Value.kReverse);
+        }else{
+            piston.set(DoubleSolenoid.Value.kForward);
+        }
+        
+    }
+
+}
